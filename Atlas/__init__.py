6831b54a19483471abc3619d1afb90997593d6e0
Matheus Xavier Silva <matheus@codealkemy.co>
2018-05-27 18:08:12 -0300

    Atlas

--- Atlas/__init__.py
+++ Atlas/__init__.py
@@ -1,5 +1,17 @@
 # Copyright (c) 2018, Matheus Xavier Silva, Aro2220
 
+
 class Atlas:
     """"This class is intended to work with the atlas list format"""
-    def __init__(self, version, comment, )
+
+    def __init__(self, version, comment):
+        self.contents = bytearray(b'\xF5\xA7')
+        if version == 1:
+            self.contents.append(1)
+        else:
+            raise ValueError
+        self.comment = comment.encode('utf-8')
+        self.contents.append(self.comment)
+
+    def add_value(self, value:bytes, identifier):
+        pass
